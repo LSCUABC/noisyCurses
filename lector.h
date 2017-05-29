c37e9f5af66d820ba962f546b6904cfb48a7a0cf
Borrego Johnson <borregs@yopmail.com>
2017-05-29 12:44:05 -0700

    Update lector.h

--- lector.h
+++ lector.h
@@ -17,29 +17,30 @@ void lector(char arch[],WINDOW *m,WINDOW *p,WINDOW *l){
 	char linea[1024],ch,can[8]="CANCION",art[8]="ARTISTA",alb[7]="ALBUM";
 
 	
-	getmaxyx(stdscr,yy,xx);
+	getmaxyx(stdscr,yy,xx); // obtenemos las dimensiones de la terminal en que estamos
 	
-	if(fexists(arch))
-		f=fopen(arch,"r");
+	if(fexists(arch)) // veryficamos que el archivo con el nombre "$arch" existe
+		f=fopen(arch,"r"); // si existe lo abrimos para lectura
 	else{
-		perror("No existe el archivo");
-		return;
+		perror("No existe el archivo"); // sino existe manda error y sale del programa
+		return;// aun no se implementa una forma segura de retornar la silda del programa ya que el main es un ciclo infinito...
 	}
 	//liz
-		getmaxyx(l,ly,lx);
-		mvwprintw(l,1,24-(strlen(can)/2),"%s",can);
-                mvwprintw(l,1,55-(strlen(art)/2),"%s",art);
-                mvwprintw(l,1,22+(xx/2)-(strlen(alb)/2),"%s",alb);
-		mvwprintw(l,ly-3,1,"<F8 ");
-		mvwprintw(l,ly-3,lx-5," F9>");
-		mvwprintw(l,ly-2,1,"[RETROCEDER]");
+		getmaxyx(l,ly,lx); //obtenemos las dimensiones actuales de la ventana de listado de canciones
+		mvwprintw(l,1,24-(strlen(can)/2),"%s",can);// Imprimimos titulos para las columnas 1) CANION
+                mvwprintw(l,1,55-(strlen(art)/2),"%s",art);// 2) ARTISTA 
+                mvwprintw(l,1,22+(xx/2)-(strlen(alb)/2),"%s",alb);// 3) ALBUM
+		mvwprintw(l,ly-3,1,"<F8 ");// Imprimimos ayuda visual para el ususarion
+		mvwprintw(l,ly-3,lx-5," F9>");// F9[AVANZAR]
+		mvwprintw(l,ly-2,1,"[RETROCEDER]");// F8[RETROCEDER]
 		mvwprintw(l,ly-2,lx-10,"[AVANZAR]");
 		
-		getmaxyx(m,my,mx);
-		mvwprintw(m,my-2,(mx/2)+7,"F10[ALEATORI0]  CTRL+Z[SALIR]");
+		getmaxyx(m,my,mx);// obtenemos las dimensiones actuales de la ventana de control
+		mvwprintw(m,my-2,(mx/2)+7,"F10[ALEATORI0]  CTRL+Z[SALIR]"); // Imprime ayuda visual para el usuario
+									    // F10[ALEATORIO] CTRL+Z[SALIR]
 
-		mvwhline(l,2,0,0,xx);
-		box(l,0,0);
+		mvwhline(l,2,0,0,xx);// imprime una linea horizontal que separa el titulo de las columnas de el listado de cancionces en la ventana de listado de canciones.
+		box(l,0,0);// volvemos a bordear a la ventana de listado ya que la linea horizontal se imprime hasta las orrilas de la ventana volviendolo visualmente desagradable. 
 
 
 
@@ -48,24 +49,23 @@ void lector(char arch[],WINDOW *m,WINDOW *p,WINDOW *l){
 	
 	do{
 				
-        fgets(n[x].a,sizeof(n[x].a),f);
-        n[x].a[strlen(n[x].a)-1]='\0';
+        fgets(n[x].a,sizeof(n[x].a),f);	// lee del archivo n caracteres y guardalos como nombre del artista en cuestion	
+        n[x].a[strlen(n[x].a)-1]='\0'; // agrega nulo a la ultima posicion del string capturado just in case..
 		
-		if(feof(f))
-			break;
+		if(feof(f))	// Si se llego al EOF entonces break
+			break;		// Break nos saca de ciclo infinito 
 			
-        fgets(n[x].al,sizeof(n[x].al),f);
-        n[x].al[strlen(n[x].al)-1]='\0';		
+		fgets(n[x].al,sizeof(n[x].al),f);//lee del archivo n caracteres y guarda como nombre de album en cuestion 
+		n[x].al[strlen(n[x].al)-1]='\0';// agrega nulo a la ultima posicion del string capturado just in case..		
+		fgets(n[x].t,sizeof(n[x].t),f);//lee del archivo n caracteres y guarda como nombre de cancion en cuestion 
+		n[x].t[strlen(n[x].t)-1]='\0';// agrega nulo a la ultima posicion del string capturado just in case..
 
-		fgets(n[x].t,sizeof(n[x].t),f);
-		n[x].t[strlen(n[x].t)-1]='\0';
-
-		fgets(linea,sizeof(linea),f);
-		n[x].m=atoi(linea);
+		fgets(linea,sizeof(linea),f);//lee del archivo 1024 caracteres y guarda como linea actual
+		n[x].m=atoi(linea);// parsa el string linea a integro si es posible guarda el resultado en min de la cancion 
 
 
-		fgets(linea,sizeof(linea),f);
-		n[x].s=atoi(linea);		
+		fgets(linea,sizeof(linea),f);//lee del archivo 1024 caracteres y guarda como linea actual
+		n[x].s=atoi(linea);// parsa el string linea a integro si es posible guarda el resultado en seg de la cancion		
 		
 		
 		mvwprintw(l,3+x,24-(strlen(n[x].t)/2),"%s",n[x].t);
@@ -73,22 +73,24 @@ void lector(char arch[],WINDOW *m,WINDOW *p,WINDOW *l){
 		mvwprintw(l,3+x,22+((xx/2)-(strlen(n[x].al)/2)),"%s",n[x].al);
 		mvwprintw(l,3+x,4,"%d)",x+1);
 		mvwprintw(l,3+x,xx-15,"%d:%d",n[x].m,n[x].s);
-		
-		wrefresh(l);
-		refresh();		
+	
+		//Imprime el Listado de canciones recien leido
+	
+		wrefresh(l);// window refresh lista
+		refresh();// refresh all windows		
 
 		x++;
 		
-		}while(1);
+		}while(1);// por siempre si no se cumple la condicion
 		
 		
-		hl=aplayer(l);
-		refresh();
+		hl=aplayer(l);// invoca a aplayer y manda su valor de retorno a int hl
+		refresh();// refresh todo
 		
-		np=nplaying(hl,n[hl-1],p,m);
-		wattroff(stdscr,A_REVERSE);
+		np=nplaying(hl,n[hl-1],p,m);// invoca nplaying y manda su valor de retorno a int np
+		wattroff(stdscr,A_REVERSE);// apaga el atributo de color inverso
 		
-		fclose(f);
+		fclose(f);// cierra el archivo en cuestion
 	}
 
 #endif
